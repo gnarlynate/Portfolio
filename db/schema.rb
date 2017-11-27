9d277f4f9c371d891d5b87722c58ba363afa1476
NBowes <nathen.bowes@shopify.com>
2017-11-27 15:55:26 -0700

    made project page railsy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127180659) do
+ActiveRecord::Schema.define(version: 20171127220450) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171127180659) do
     t.string   "language"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "img"
   end
 
 end
